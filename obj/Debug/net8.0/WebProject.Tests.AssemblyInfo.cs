38c14c0d2f93bca9b73e0fd64259af685d09cabf
Truong Nguyen Dieu Linh <dieulinhsaomai36@gmail.com>
2024-12-31 23:05:54 +0700

    DieuLinh_UnitTest4

--- obj/Debug/net8.0/WebProject.Tests.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProject.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProject.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e63eb90f05edb15642783da5a5498468d5aea02")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProject.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProject.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
